f7a07ecace144184ff53d85880f54a28d231a43d
Tingyan Shen <tingyan.shen@gmail.com>
2021-06-30 15:10:50 +0800

    Fixed using broker client rsocket requester

--- magoko-archive/magoko-archive-client/src/main/java/org/deepinthink/magoko/archive/client/config/ArchiveClientBrokerConfiguration.java
+++ magoko-archive/magoko-archive-client/src/main/java/org/deepinthink/magoko/archive/client/config/ArchiveClientBrokerConfiguration.java
@@ -18,7 +18,6 @@ package org.deepinthink.magoko.archive.client.config;
 import static org.deepinthink.magoko.archive.client.ArchiveClientConstants.ARCHIVE_CLIENT_RSOCKET_REQUESTER_BEAN_NAME;
 import static org.deepinthink.magoko.broker.client.BrokerClientConstants.BROKER_CLIENT_RSOCKET_REQUESTER_BEAN_NAME;
 
-import java.util.Objects;
 import org.deepinthink.magoko.archive.client.condition.ConditionalOnArchiveClientBroker;
 import org.deepinthink.magoko.broker.client.context.BrokerClientRSocketRequesterBootstrap;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -27,7 +26,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.messaging.rsocket.RSocketStrategies;
 
 @SpringBootConfiguration(proxyBeanMethods = false)
 @ConditionalOnClass(BrokerClientRSocketRequesterBootstrap.class)
@@ -37,12 +35,7 @@ public class ArchiveClientBrokerConfiguration {
   @Bean(ARCHIVE_CLIENT_RSOCKET_REQUESTER_BEAN_NAME)
   @ConditionalOnMissingBean(name = ARCHIVE_CLIENT_RSOCKET_REQUESTER_BEAN_NAME)
   public RSocketRequester archiveClientBrokerRSocketRequester(
-      @Qualifier(BROKER_CLIENT_RSOCKET_REQUESTER_BEAN_NAME) RSocketRequester requester,
-      RSocketStrategies rSocketStrategies) {
-    return RSocketRequester.wrap(
-        Objects.requireNonNull(requester.rsocket()),
-        requester.dataMimeType(),
-        requester.metadataMimeType(),
-        rSocketStrategies);
+      @Qualifier(BROKER_CLIENT_RSOCKET_REQUESTER_BEAN_NAME) RSocketRequester requester) {
+    return requester;
   }
 }
