abff23531792556d7b59492af11c6427789b7235
Tingyan Shen <tingyan.shen@gmail.com>
2021-07-09 18:27:43 +0800

    Add basic @rsocket routing-forward metadata frames (#91)

---
+++ magoko-broker/magoko-broker-core/src/main/java/org/deepinthink/magoko/broker/core/routing/codec/RSocketRoutingFrameHeaderCodec.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2021-present deepinthink. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.deepinthink.magoko.broker.core.routing.codec;
+
+import io.netty.buffer.ByteBuf;
+import org.deepinthink.magoko.broker.core.routing.RSocketRoutingFrameType;
+
+public class RSocketRoutingFrameHeaderCodec {
+
+  public static int flags(ByteBuf byteBuf) {
+    return 0;
+  }
+
+  public static RSocketRoutingFrameType frameType(ByteBuf byteBuf) {
+    return null;
+  }
+}
